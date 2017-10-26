3b4b4826a43c7a68a36e103e46f5804bff8a7729
alex <a1s2d3198@gmail.com>
2017-10-27 01:13:59 +0800

    hw2 v1.0.1 (.sh tab refine)

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 if [ ! -f "./model/xgb.model" ]; then
 	python3 src/hw2_xgboost.py --train \
-							   --train-data-path=$3 \
-							   --train-label-path=$4 \
-							   --test-data-path=$5 \
-							   --output-path=$6
+					--train-data-path=$3 \
+					--train-label-path=$4 \
+					--test-data-path=$5 \
+					--output-path=$6
 fi
 python3 src/hw2_xgboost.py --infer \
-						   --train-data-path=$3 \
-						   --train-label-path=$4 \
-						   --test-data-path=$5 \
-						   --output-path=$6
+				--train-data-path=$3 \
+				--train-label-path=$4 \
+				--test-data-path=$5 \
+				--output-path=$6
--- hw2/hw2_logistic.sh
+++ hw2/hw2_logistic.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 if [ ! -f "./params/w" ] || [ ! -f "./params/b" ]; then
 	python3 src/hw2.py --train --logistic \
-					   --train-data-path=$3 \
-					   --train-label-path=$4 \
-					   --test-data-path=$5 \
-					   --output-path=$6
+			--train-data-path=$3 \
+			--train-label-path=$4 \
+			--test-data-path=$5 \
+			--output-path=$6
 fi
 python3 src/hw2.py --infer --logistic \
-				   --train-data-path=$3 \
-				   --train-label-path=$4 \
-				   --test-data-path=$5 \
-				   --output-path=$6
+		--train-data-path=$3 \
+		--train-label-path=$4 \
+		--test-data-path=$5 \
+		--output-path=$6
